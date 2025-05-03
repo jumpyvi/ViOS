430a7946eb25e34bc7d11481f2c3435ed19e7646
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-05-03 12:05:20 -0400

    Update spinner branding

--- build_files/branding/branding-ublue.sh
+++ build_files/branding/branding-ublue.sh
@@ -4,3 +4,12 @@ set -ouex pipefail
 
 cp /ctx/branding/ublue-logo.png /usr/share/pixmaps/fedora-gdm-logo.png
 cp /ctx/branding/bootc.png /usr/share/plymouth/themes/spinner/watermark.png
+
+cat <<EOF > "/usr/share/plymouth/plymouthd.defaults"
+# ViOS custom spinner
+[Daemon]
+Theme=spinner
+ShowDelay=0
+DeviceTimeout=8
+UseSimpledrm=1
+EOF
