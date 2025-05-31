816610d89ce6a1c5f8022751b365cab2ca37e98b
JumpyVi <jumpyvi@outlook.com>
2025-05-31 17:15:40 -0400

    Move to bluefin stable

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,25 +20,18 @@ function echo_group() {
 if [[ "$edition" == "workstation" ]]; then
     cp /ctx/packages/repos/ghostty.repo /etc/yum.repos.d/ghostty.repo
     cp /ctx/packages/repos/nordvpn.repo /etc/yum.repos.d/nordvpn.repo
-    cp /ctx/packages/repos/vscode.repo /etc/yum.repos.d/vscode.repo
-    cp /ctx/packages/repos/docker.repo /etc/yum.repos.d/docker.repo
-    echo_group /ctx/packages/utils.sh
-    echo_group /ctx/packages/containers.sh
-    echo_group /ctx/packages/gnome-extensions.sh
-    echo_group /ctx/packages/virt.sh
     echo_group /ctx/branding/branding-ublue.sh
+    dnf5 remove ncurses-term -y
     dnf5 install -y ghostty nordvpn code
     dnf remove -y ptyxis
 
 elif [[ "$edition" == "gaming" ]]; then
     cp /ctx/packages/repos/nordvpn.repo /etc/yum.repos.d/nordvpn.repo
     cp /ctx/packages/repos/docker.repo /etc/yum.repos.d/docker.repo
-    echo_group /ctx/packages/utils.sh
     echo_group /ctx/packages/containers.sh
     echo_group /ctx/packages/cockpit.sh
     dnf5 install -y nordvpn
     dnf remove -y ptyxis
-    nvidia-ctk runtime configure --runtime=docker
 
 else
     echo "Invalid edition specified. Please use 'workstation' or 'gaming'."
