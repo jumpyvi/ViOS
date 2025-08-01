deb8d3d4b74450dfa2c4049c1709f60eb89574b5
JumpyVi <jumpyvi@outlook.com>
2025-08-01 17:19:06 -0400

    Containerfile cleanup

--- Containerfile
+++ Containerfile
@@ -1,15 +1,20 @@
 # Allow build scripts to be referenced without being copied into the final image
+ARG BASE_IMAGE=none
+
 FROM scratch AS ctx
 COPY build_files /
 
+
 # Base Image
-FROM ghcr.io/ublue-os/bluefin-dx:stable
+FROM ${BASE_IMAGE}
+
+ARG TYPE=none
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh "workstation" && \
+    /ctx/build.sh "${TYPE}" && \
     ostree container commit
     
 
--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,8 @@ set -ouex pipefail
 
 edition="$1"
 
+echo "edition is $edition"
+
 if [[ "$edition" == "workstation" ]]; then
     cp /ctx/packages/repos/ghostty.repo /etc/yum.repos.d/ghostty.repo
     cp /ctx/packages/repos/cachykernel.repo /etc/yum.repos.d/cachykernel.repo
@@ -11,13 +13,12 @@ if [[ "$edition" == "workstation" ]]; then
     /ctx/branding/branding-ublue.sh
     dnf5 remove ptyxis ncurses-term -y
     dnf remove -y 'gnome-shell-extension*'
-    dnf5 install -y ghostty nautilus-python nordvpn code
+    dnf5 install -y ghostty nautilus-python code
 
 elif [[ "$edition" == "gaming" ]]; then
     cp /ctx/packages/repos/docker.repo /etc/yum.repos.d/docker.repo
     /ctx/packages/containers.sh
     /ctx/packages/cockpit.sh
-    dnf5 install -y nordvpn
     dnf remove -y ptyxis
 
 elif [[ "$edition" == "server" ]]; then
