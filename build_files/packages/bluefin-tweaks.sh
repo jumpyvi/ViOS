faaa4ff28ce0ad2c26119634dcc86e88f113ed42
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-11-17 16:07:16 -0500

    Fix removals

--- build_files/packages/bluefin-tweaks.sh
+++ build_files/packages/bluefin-tweaks.sh
@@ -13,17 +13,8 @@ extra=(
   "nautilus-open-any-terminal"
 )
 
-removals=(
-  "ptyxis"
-  "fish"
-  "bluefin-fastfetch"
-  "bluefin-faces"
-  "fastfetch"
-  "ghostty-nautilus"
-)
-
 dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf remove -y 'gnome-shell-extension*'
-dnf5 remove -y ${removals[@]}
-
 dnf5 install -y --enablerepo=terra ${extra[@]}
+
+dnf remove -y 'gnome-shell-extension*'
+dnf5 remove -y ptyxis fish bluefin-fastfetch bluefin-faces fastfetch ghostty-nautilus
