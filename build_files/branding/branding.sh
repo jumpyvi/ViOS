f8acbc71623a143c9d5cb235719ff2d363b9f1ba
JumpyVi <jumpyvi@outlook.com>
2025-11-22 22:08:22 -0500

    Remove some svg

--- build_files/branding/branding.sh
+++ build_files/branding/branding.sh
@@ -5,7 +5,6 @@ set -ouex pipefail
 # GDM Logo by @furtivetardigrade, @kikaraage, @jf8130, and @chandeleer
 cp /ctx/branding/gdm-logo.png /usr/share/pixmaps/fedora-gdm-logo.png
 cp /ctx/branding/bootc.png /usr/share/plymouth/themes/spinner/watermark.png
-cp /ctx/branding/better-bluetooth.svg /usr/share/icons/hicolor/scalable/status/blueman-tray.svg
 
 cat <<EOF > "/usr/share/plymouth/plymouthd.defaults"
 # ViOS custom spinner
