441ccbaa46d838902edf0e3a6404d3f800b0eff4
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-12-01 22:11:52 -0500

    Add sway to multimedia

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,7 +16,7 @@ elif [[ "$edition" == "multimedia" ]]; then
     /ctx/packages/cockpit.sh
     /ctx/packages/virt.sh
     dnf remove -y ptyxis
-    dnf install -y borgbackup
+    dnf install -y borgbackup sway
 
 else
     echo "Invalid edition specified. Please use 'workstation' or 'multimedia'."
