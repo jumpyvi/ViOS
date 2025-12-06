de4734ed553a5875b2406a648da7a744a5781398
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-12-06 13:38:32 -0500

    Reduce amount of packages in bluefin tweaks
    Removed 'nautilus-python' and 'nautilus-open-any-terminal' from the extra packages list. Simplified the installation command by removing the repository specification.

--- build_files/packages/bluefin-tweaks.sh
+++ build_files/packages/bluefin-tweaks.sh
@@ -3,14 +3,9 @@
 set -ouex pipefail
 
 extra=(
-  "nautilus-python"
   "nm-connection-editor"
   "evolution"
   "evolution-ews"
-  "nautilus-open-any-terminal"
 )
 
-dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf5 install -y --enablerepo=terra ${extra[@]}
-
-dnf remove -y 'gnome-shell-extension*'
+dnf5 install -y ${extra[@]}
