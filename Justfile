e3b3ce4158385251ad9dccaa4b982dd738d3e776
JumpyVi <jumpyvi@outlook.com>
2026-01-03 16:56:27 -0500

    libvirt testing

--- Justfile
+++ Justfile
@@ -3,6 +3,3 @@ _default:
 
 build-workstation:
     podman build -f Containerfile --build-arg TYPE="workstation" --build-arg BASE_IMAGE=$(grep '^WORKSTATION_IMAGE=' images.conf | cut -d'=' -f2-) -t local-boot:latest
-
-build-multimedia:
-    podman build -f Containerfile --build-arg TYPE="multimedia" --build-arg BASE_IMAGE=$(grep '^MULTIMEDIA_IMAGE=' images.conf | cut -d'=' -f2-) -t local-boot:latest
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,21 +2,9 @@
 
 set -ouex pipefail
 
-edition="$1"
-
-
-if [[ "$edition" == "workstation" ]]; then
-    /ctx/branding/branding.sh
-    /ctx/packages/kernel.sh
-    /ctx/packages/bluefin-tweaks.sh
-
-elif [[ "$edition" == "multimedia" ]]; then
-    /ctx/packages/cockpit.sh
-    dnf remove -y ptyxis
-
-else
-    echo "Invalid edition specified. Please use 'workstation' or 'multimedia'."
-    exit 1
-fi
-
-
+dnf -y copr enable ublue-os/packages
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install \
+  libvirt \
+  libvirt-nss \
+  virt-install \
+  ublue-os-libvirt-workarounds
