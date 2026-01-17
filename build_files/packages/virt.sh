ae078c32a347b74a5a583d6c2e186fb7cc2586f3
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2026-01-16 21:09:57 -0500

    Remove qemu, keep libvirt

--- build_files/packages/virt.sh
+++ build_files/packages/virt.sh
@@ -4,19 +4,20 @@ set -ouex pipefail
 
 dnf -y copr enable ublue-os/packages
 dnf5 install -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages --skip-unavailable \
-    edk2-ovmf \
-    genisoimage \
     libvirt \
-    libvirt-nss \
-    virt-manager \
-    virt-v2v \
-    qemu-char-spice \
-    qemu-device-display-virtio-gpu \
-    qemu-device-display-virtio-vga \
-    qemu-device-usb-redirect \
-    qemu-img \
-    qemu-system-x86-core \
-    qemu-user-binfmt \
-    qemu-user-static \
-    qemu \
     ublue-os-libvirt-workarounds
+    # edk2-ovmf \
+    # genisoimage \
+    # libvirt-nss \
+    # virt-manager \
+    # virt-v2v \
+    # qemu-char-spice \
+    # qemu-device-display-virtio-gpu \
+    # qemu-device-display-virtio-vga \
+    # qemu-device-usb-redirect \
+    # qemu-img \
+    # qemu-system-x86-core \
+    # qemu-user-binfmt \
+    # qemu-user-static \
+    # qemu \
+    
