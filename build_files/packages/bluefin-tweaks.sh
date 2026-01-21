81732e84a16c64cabf75748049881208b0881fc5
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2026-01-21 14:13:25 -0500

    Remove bluefin-tweaks content

--- build_files/packages/bluefin-tweaks.sh
+++ build_files/packages/bluefin-tweaks.sh
@@ -2,11 +2,4 @@
 
 set -ouex pipefail
 
-extra=(
-  "nm-connection-editor"
-  "evolution"
-  "evolution-ews"
-)
-
-dnf5 install -y ${extra[@]}
-dnf5 remove -y input-remapper
+echo "Nothing to do..."
